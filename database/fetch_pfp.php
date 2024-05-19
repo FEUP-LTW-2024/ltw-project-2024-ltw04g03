6a640fd3f15e2e3f8808c2d4361d4208eca9dbba
Oferecido <ricardo.parreira.tic@gmail.com>
2024-05-19 05:28:42 +0100

    profile picture finalized. already showing on profile

--- database/fetch_pfp.php
+++ database/fetch_pfp.php
@@ -4,9 +4,12 @@
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     $stmt = $pdo->prepare("SELECT profile_image FROM User WHERE username = :username");
-    $stmt->bindParam(':username', $_SESSION['username'] );
+    $stmt->bindParam(':username', $username );
     $stmt->execute();
 
     $profile_image = $stmt->fetchColumn();
+    if (!$profile_image) {
+        $profile_image = '../docs/profile_images/default_pfp.jpg'; // default image
+    }
     //echo $profile_image;
 ?>
